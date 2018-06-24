6727da756ae5b2342ebfe1b650da30dcf27fbcfc
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-06-24 21:58:16 +0800

    Early return

--- timeago.go
+++ timeago.go
@@ -5,22 +5,35 @@ import (
 	"time"
 )
 
+// TimeAgo returns a string
+// that calculates time ago
 func TimeAgo(ago int64) string {
-
 	timeDiff := (time.Now().Unix() - ago)
+
 	// TODO:: this should be fixed
 	if timeDiff < 60 {
-		return fmt.Sprintf("%ds", timeDiff)
-	} else if timeDiff < 3600 {
-		return fmt.Sprintf("%dm", int64(timeDiff/60))
-	} else if timeDiff < (86400) {
-		return fmt.Sprintf("%dh", int64(timeDiff/3600))
-	} else if timeDiff < (604800) {
-		return fmt.Sprintf("%dd", int64(timeDiff/86400))
-	} else if timeDiff < (2628000) {
-		return fmt.Sprintf("%dw", int64(timeDiff/604800))
-	} else if timeDiff < (31536000) {
-		return fmt.Sprintf("%dM", int64(timeDiff/2628000))
+		return fmt.Sprintf("%d s", timeDiff)
 	}
-	return fmt.Sprintf("%dy", int64(timeDiff/31536000))
+
+	if timeDiff < 3600 {
+		return fmt.Sprintf("%d min", int64(timeDiff/60))
+	}
+
+	if timeDiff < (86400) {
+		return fmt.Sprintf("%d hr", int64(timeDiff/3600))
+	}
+
+	if timeDiff < (604800) {
+		return fmt.Sprintf("%d d", int64(timeDiff/86400))
+	}
+
+	if timeDiff < (2628000) {
+		return fmt.Sprintf("%d wk", int64(timeDiff/604800))
+	}
+
+	if timeDiff < (31536000) {
+		return fmt.Sprintf("%d Mo", int64(timeDiff/2628000))
+	}
+
+	return fmt.Sprintf("%d y", int64(timeDiff/31536000))
 }
